69a238fe1e1e12fdc1b5021080e2f3c39ce68ae1
alvinscheibe <alvin.scheibe@gmail.com>
2022-09-30 23:15:03 -0300

    Adding fontawesome icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes';
+import { Footer, Navbar } from '../components';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute={'class'}>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+
+      <Script src={'https://kit.fontawesome.com/f50c36a3a1.js'} crossOrigin={'anonymous'} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp
