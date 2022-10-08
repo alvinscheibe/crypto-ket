5f3394cbdc1ed957ea28d6eaa8d8e8393afd0ee1
alvinscheibe <alvin.scheibe@gmail.com>
2022-10-07 21:38:58 -0300

    NFTMarketplace smart contract

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -24,7 +24,7 @@ contract NFTMarketplace is ERC721URIStorage {
         bool sold;
     }
 
-    event MarketItemCreated (
+    event MarketItemCreated(
         uint256 indexed tokenId,
         address seller,
         address owner,
@@ -32,7 +32,17 @@ contract NFTMarketplace is ERC721URIStorage {
         bool sold
     );
 
-    constructor () {
+    constructor() {
         owner = payable(msg.sender);
+    }
+
+    function updateListingPrice(uint _listingPrice) public payable {
+        require(owner == msg.sender, "Only marketplace owner can update the listing price");
+
+        listingPrice = _listingPrice;
+    }
+
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
     }
 }
